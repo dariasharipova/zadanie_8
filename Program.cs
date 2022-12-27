617edb121d687b6cb22808f5e51bc823d177cf4a
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-12-27 18:17:38 +0500

    исправлена ошибка и стиль 7.3

--- Program.cs
+++ Program.cs
@@ -34,85 +34,85 @@ static bool koordinati(ref double x, ref double y)
 Console.WriteLine(koordinati(ref x, ref y));
 
 Console.WriteLine("Введите позицию белого коня");
-var whitehorsepositin = Console.ReadLine();
+var WhiteKnightPosition = Console.ReadLine();
 
-static void decodeposition(string position, out int row, out int colomn)
+static void DecodePosition(string position, out int row, out int colomn)
 {
     row = int.Parse(position[1].ToString());
     colomn = (int)position[0] - 0x60;
 
 }
-static bool checkposition(string position)
+static bool CheckPosition(string position)
 {
     int r, c;
-    decodeposition(position,out r,out c);
-    return r>=1 & r<=8;
+    DecodePosition(position,out r,out c);
+    return r>=1 && r<=8 && c >=1 && c <= 8;
 }
 
-if (!checkposition(whitehorsepositin))
+if (!CheckPosition(WhiteKnightPosition))
 {
     Console.WriteLine("Позиция белой фигуры неккоректна");
     return;
 }
 
 Console.WriteLine("Введите позицию черного ферзя");
-var blackferzpositin = Console.ReadLine();
+var BlackQueenPosition = Console.ReadLine();
 
-if (!checkposition(blackferzpositin) || (whitehorsepositin == blackferzpositin))
+if (!CheckPosition(BlackQueenPosition) || (WhiteKnightPosition == BlackQueenPosition ))
 {
     Console.WriteLine("Позиция черной фигуры неккоректна");
     return;
 }
 
-if(whitehorseunderstrike(whitehorsepositin,blackferzpositin))
+if(WhiteHorseUnderStrike(WhiteKnightPosition,BlackQueenPosition))
 {
     Console.WriteLine("Белый конь под ударом");
 }
-if (blackferzunderstrike(blackferzpositin,whitehorsepositin))
+if (BlackFerzUnderStrike(BlackQueenPosition,WhiteKnightPosition))
 {
     Console.WriteLine("Черный ферзь под ударом");
 }
 
-static bool whitehorseunderstrike(string whitehorseposition, string blackferzposition)
+static bool WhiteHorseUnderStrike(string WhiteKnightPosition, string BlackQueenPosition)
 {
     int whr,whc,bfr,bfc;
-    decodeposition(whitehorseposition,out whr,out whc);
-    decodeposition(blackferzposition,out bfr, out bfc);
-    return ((whr == bfr || whc == bfc) || (Math.Abs(whr - bfr) == Math.Abs(whc - bfc))) & (whitehorseposition != blackferzposition);
+    DecodePosition(WhiteKnightPosition,out whr,out whc);
+    DecodePosition(BlackQueenPosition,out bfr, out bfc);
+    return ((whr == bfr || whc == bfc) || (Math.Abs(whr - bfr) == Math.Abs(whc - bfc))) && (WhiteKnightPosition != BlackQueenPosition);
 
 }
 
-static bool blackferzunderstrike(string blackferzposition,string whitehorseposition)
+static bool BlackFerzUnderStrike(string BlackQueenPosition,string WhiteKnightPosition)
 {
     int bfr, bfc, whr,whc;
-    decodeposition(blackferzposition, out bfr, out bfc);
-    decodeposition(whitehorseposition, out whr, out whc);
-    return ((Math.Abs(bfr - whr) == 1) & (Math.Abs(bfc - whc) == 2)) || ((Math.Abs(bfc - whc) == 1) & (Math.Abs(bfr - whr) ==2));
+    DecodePosition(BlackQueenPosition, out bfr, out bfc);
+    DecodePosition(WhiteKnightPosition, out whr, out whc);
+    return ((Math.Abs(bfr - whr) == 1) && (Math.Abs(bfc - whc) == 2)) || ((Math.Abs(bfc - whc) == 1) && (Math.Abs(bfr - whr) ==2));
 
 }
 Console.WriteLine("Введите ход белого коня");
-var whitehorsemove = Console.ReadLine();
+var WhiteKnightMove = Console.ReadLine();
 
-static bool correctwhitehorsemove(string horsestart, string horseend)
+static bool CorrectWhiteHorseMove(string horsestart, string horseend)
 {
     int whrs, whcs, whre, whce;
-    decodeposition(horsestart,out whrs, out whcs);
-    decodeposition(horseend, out whre, out whce);
-    return ((Math.Abs(whrs - whre) == 1) & (Math.Abs(whcs - whce) == 2)) || ((Math.Abs(whrs - whre) == 1) & (Math.Abs(whcs - whce) ==2));
+    DecodePosition(horsestart,out whrs, out whcs);
+    DecodePosition(horseend, out whre, out whce);
+    return ((Math.Abs(whrs - whre) == 1) && (Math.Abs(whcs - whce) == 2)) || ((Math.Abs(whce - whcs) == 1) && (Math.Abs(whre - whrs) ==2));
 
 }
-if ((!correctwhitehorsemove(whitehorsepositin, whitehorsemove)))
+if ((!CorrectWhiteHorseMove(WhiteKnightPosition, WhiteKnightMove)))
 {
     Console.WriteLine("Ход коня неверный. Конь так не ходит.");
 }
-else if (whitehorseunderstrike(whitehorsemove,blackferzpositin))
+else if (WhiteHorseUnderStrike(WhiteKnightMove,BlackQueenPosition))
 {
     Console.WriteLine("Конь под ударом");
 }
 else
 {
     Console.WriteLine("Ход коня верный");
-    if (whitehorsemove == blackferzpositin)
+    if (WhiteKnightMove == BlackQueenPosition)
     {
         Console.WriteLine("Черный ферзь срублен");
     }
